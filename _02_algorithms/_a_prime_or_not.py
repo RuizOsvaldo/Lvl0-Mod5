f210eabbb2aa1cde76c3f19e7125e84166e9aa7d
Osvaldo <osvaldo.ruiz@jointheleague.org>
2022-06-29 19:19:59 -0700

    Completed prime or not

--- _02_algorithms/_a_prime_or_not.py
+++ _02_algorithms/_a_prime_or_not.py
@@ -8,8 +8,19 @@ from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     # TODO)
     #  1. Ask the user for a number
+    window = Tk()
+    window.withdraw()
+    number = simpledialog.askinteger(None, "Type in a number.")
     #  2. Use a for loop, if statement, and modulo to find if the number
     #     is prime.
+
+    is_prime = True
+    for i in range(number):
+        if i > 1 and number % i == 0:
+            is_prime = False
+            break
+
     #  3. If the number is divisible by any number other than 1 or itself,
     #     the number is not prime.
-    pass
+
+    print(is_prime)
